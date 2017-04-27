41f7cb087f2fa21052ff04e8423a9de92c324228
Ekta Khanna and Jimmy Yih <ekhanna+jyih@pivotal.io>
2017-04-27 15:23:26 -0700

    Fixing storage tests with ORCA turned ON[#138725549]
    For appendonly_read_check, setting `optimizer_disable_missing_stats_collection=on`
    to hide the additional NOTICES/HINTS produced by ORCA.
    
    For uao/uaocs crash on update tests, we are setting `optimizer=off` as ORCA does
    not go through the code path intended for these tests.

--- src/test/tinc/tincrepo/mpp/gpdb/tests/storage/uao/uao_faultinjection/sql/uaocs_crash_update_intran1.sql
+++ src/test/tinc/tincrepo/mpp/gpdb/tests/storage/uao/uao_faultinjection/sql/uaocs_crash_update_intran1.sql
@@ -2,5 +2,11 @@
 SET gp_create_table_random_default_distribution=off;
 -- end_ignore
 BEGIN;
+-- start_ignore
+-- In case of UPDATE, ORCA does not go through the ExecUpdate code path while
+-- the purpose of this test is to PANIC in the appendonly_update code path.
+-- Therefore, we turn off ORCA for this test.
+SET optimizer=off;
+-- end_ignore
 UPDATE foo set b = 0 ;
 COMMIT;
