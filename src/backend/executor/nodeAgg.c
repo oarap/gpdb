ff10223cc4f65a1366380ccc2249e94132cd2f1c
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-09-29 11:16:17 +0300

    Allow aggregate functions to be VARIADIC.
    Backported from PostgreSQL 9.4, because subsequent backported commits depend
    on it.
    
    commit 0d3f4406dfa00d848711fdb4af53be663ffc7d0f
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Date:   Tue Sep 3 17:08:38 2013 -0400
    
        Allow aggregate functions to be VARIADIC.
    
        There's no inherent reason why an aggregate function can't be variadic
        (even VARIADIC ANY) if its transition function can handle the case.
        Indeed, this patch to add the feature touches none of the planner or
        executor, and little of the parser; the main missing stuff was DDL and
        pg_dump support.
    
        It is true that variadic aggregates can create the same sort of ambiguity
        about parameters versus ORDER BY keys that was complained of when we
        (briefly) had both one- and two-argument forms of string_agg().  However,
        the policy formed in response to that discussion only said that we'd not
        create any built-in aggregates with varying numbers of arguments, not that
        we shouldn't allow users to do it.  So the logical extension of that is
        we can allow users to make variadic aggregates as long as we're wary about
        shipping any such in core.
    
        In passing, this patch allows aggregate function arguments to be named, to
        the extent of remembering the names in pg_proc and dumping them in pg_dump.
        You can't yet call an aggregate using named-parameter notation.  That seems
        like a likely future extension, but it'll take some work, and it's not what
        this patch is really about.  Likewise, there's still some work needed to
        make window functions handle VARIADIC fully, but I left that for another
        day.
    
        initdb forced because of new aggvariadic field in Aggref parse nodes.

--- src/backend/executor/nodeAgg.c
+++ src/backend/executor/nodeAgg.c
@@ -1934,6 +1934,7 @@ ExecInitAgg(Agg *node, EState *estate, int eflags)
 		/* build expression trees using actual argument & result types */
 		build_aggregate_fnexprs(inputTypes,
 								numArguments,
+								aggref->aggvariadic,
 								aggtranstype,
 								aggref->aggtype,
 								transfn_oid,
