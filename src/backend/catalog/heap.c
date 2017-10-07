0e75c0aa418e57a65caa7d15f790dadc5ce99a41
Heikki Linnakangas <hlinnakangas@pivotal.io>
2017-10-07 16:47:47 +0300

    Backport upstream bugfix related to Window functions.
    The test case added to the regression suite actually seems to work on
    GPDB even without this, but nevertheless seems like a good idea to pick
    it now, since we have the code it affected. Also, I'm about to backport
    more stuff that depend on this.
    
    commit c1d9579dd8bf3c921ca6bc2b62c40da6d25372e5
    Author: Tom Lane <tgl@sss.pgh.pa.us>
    Date:   Tue Jul 12 18:23:55 2011 -0400
    
        Avoid listing ungrouped Vars in the targetlist of Agg-underneath-Window.
    
        Regular aggregate functions in combination with, or within the arguments
        of, window functions are OK per spec; they have the semantics that the
        aggregate output rows are computed and then we run the window functions
        over that row set.  (Thus, this combination is not really useful unless
        there's a GROUP BY so that more than one aggregate output row is possible.)
        The case without GROUP BY could fail, as recently reported by Jeff Davis,
        because sloppy construction of the Agg node's targetlist resulted in extra
        references to possibly-ungrouped Vars appearing outside the aggregate
        function calls themselves.  See the added regression test case for an
        example.
    
        Fixing this requires modifying the API of flatten_tlist and its underlying
        function pull_var_clause.  I chose to make pull_var_clause's API for
        aggregates identical to what it was already doing for placeholders, since
        the useful behaviors turn out to be the same (error, report node as-is, or
        recurse into it).  I also tightened the error checking in this area a bit:
        if it was ever valid to see an uplevel Var, Aggref, or PlaceHolderVar here,
        that was a long time ago, so complain instead of ignoring them.
    
        Backpatch into 9.1.  The failure exists in 8.4 and 9.0 as well, but seeing
        that it only occurs in a basically-useless corner case, it doesn't seem
        worth the risks of changing a function API in a minor release.  There might
        be third-party code using pull_var_clause.

--- src/backend/catalog/heap.c
+++ src/backend/catalog/heap.c
@@ -2648,7 +2648,9 @@ StoreRelCheck(Relation rel, char *ccname, char *ccbin)
 	 * in check constraints; it would fail to examine the contents of
 	 * subselects.
 	 */
-	varList = pull_var_clause(expr, PVC_REJECT_PLACEHOLDERS);
+	varList = pull_var_clause(expr,
+							  PVC_REJECT_AGGREGATES,
+							  PVC_REJECT_PLACEHOLDERS);
 	keycount = list_length(varList);
 
 	if (keycount > 0)
@@ -2891,7 +2893,9 @@ AddRelationConstraints(Relation rel,
 			List	   *vars;
 			char	   *colname;
 
-			vars = pull_var_clause(expr, PVC_REJECT_PLACEHOLDERS);
+			vars = pull_var_clause(expr,
+								   PVC_REJECT_AGGREGATES,
+								   PVC_REJECT_PLACEHOLDERS);
 
 			/* eliminate duplicates */
 			vars = list_union(NIL, vars);
